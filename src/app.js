2d01e86aa2c42cc323c5253b5a9274c1c19543ed
wesdevclimb <wesleyjohnson.stu@gmail.com>
2016-10-28 07:17:24 -0400

    Included Angular in the index.html and removed the views folder along with the pug templating engine.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
-var pug = require("pug");
 var express = require('express');
 var app = express();
 var PORT = 9000;
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-
 app.get('/', function(req, res){
-  res.render('index');
+  
 });
 
 app.listen(PORT, function(){
