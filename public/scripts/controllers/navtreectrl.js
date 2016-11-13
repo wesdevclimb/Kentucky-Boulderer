19658c27bcb8c18c590b89b0c9d4945ebfb9a6ae
wesdevclimb <wesleyjohnson.stu@gmail.com>
2016-11-12 21:59:59 -0500

    the nav tree controller now filters the raw data that is added to the scope and can display all child boulders of a parent area.

--- public/scripts/controllers/navtreectrl.js
+++ public/scripts/controllers/navtreectrl.js
@@ -3,13 +3,30 @@
 angular.module('guidebookApp')
 .controller('navtreeCtrl', function($scope, dataService) {
 
-    dataService.getAreas(function(response) {
-      var data = response.data;
-      $scope.areas = data.areas;
-      data.areas.forEach(function(area) {
-        console.log(area.name);
-      });
+    dataService.getAreas(function(res) {
+      var data = res.data;
       console.log(data);
+
+      var filteredData = [];
+
+      data.areas.forEach(function(area) {
+        filteredData.push(area);
+      });
+
+      filteredData.forEach(function(area) {
+        data.boulders.forEach(function(boulder) {
+          if(area._id === boulder.parentArea) {
+            area.boulders.push(boulder);
+          } else {
+            console.log("your plan did not work asshole");
+          }
+        });
+      });
+
+      console.log(filteredData);
+
+      $scope.areas = filteredData;
+
     });
 
 });
